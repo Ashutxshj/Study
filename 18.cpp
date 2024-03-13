21ac238f1eb61d69234b925e7c25f6d06ae0c09d
Ashutxshj <ashutosh06066@gmail.com>
2024-03-13 13:46:55 +0530

    askhat

---
+++ 18.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+class akshat{
+    public:
+    void display(int a){
+        cout<<a<<endl;
+    }
+    void display(int a,int b){
+        cout<<a<<" "<<b<<endl;
+    }
+};
+int main(){
+akshat a;
+a.display(2);
+a.display(2,3);
+}
